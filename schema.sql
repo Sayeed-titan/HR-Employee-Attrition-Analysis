0a6e4e6a7a673bfb5702896b44f119f7d0bbd64e
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-11-22 20:00:58 +0600

    Create Tables

--- schema.sql
+++ schema.sql
@@ -10,3 +10,23 @@ GO
 USE HRAnalyticsDB;
 GO
 
+-- Create Tables
+CREATE TABLE Departments (
+    DeptID INT PRIMARY KEY,
+    DeptName VARCHAR(50),
+    ManagerID INT
+);
+
+CREATE TABLE Employees (
+    EmpID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    DeptID INT FOREIGN KEY REFERENCES Departments(DeptID),
+    HireDate DATE,
+    Salary DECIMAL(10,2),
+    Age INT,
+    Gender VARCHAR(10),
+    JobLevel VARCHAR(20),
+    YearsAtCompany INT,
+    PerformanceRating INT, -- 1-5
+    Attrition BIT -- 0=Active, 1=Left
+);
