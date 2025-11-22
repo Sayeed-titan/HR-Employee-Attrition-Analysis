63bf797707ef30c27631a7f7c5ec53fa7a9b5fbf
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-11-22 19:47:24 +0600

    Create database

---
+++ schema.sql
@@ -0,0 +1,12 @@
+-- =====================================================
+-- PROJECT Name: HR Employee Attrition Analysis
+-- Objective: Identify patterns in employee turnover
+-- Skills: SQL Analysis, Business Insights, Reporting
+-- =====================================================
+
+-- Create Database
+CREATE DATABASE HRAnalyticsDB;
+GO
+USE HRAnalyticsDB;
+GO
+
